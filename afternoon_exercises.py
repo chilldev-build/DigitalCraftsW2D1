a15abfb9f75901a053aa4602a0f14edce2d0aa2b
Chris Hill <chrishillbuilds@gmail.com>
2019-09-04 12:36:15 -0400

    A few changes to file

--- afternoon_exercises.py
+++ afternoon_exercises.py
@@ -81,63 +81,70 @@
 # S => 5
 # T => 7
 
-str = 'This is my absolute test string to see if my Leet cipher is working'
+#str = 'This is my absolute test string to see if my Leet cipher is working'
+#
+#def my_function_a(x):
+#  strA = x.replace('a','4')
+#  return strA
+#
+#strA = my_function_a(str)
+#
+##print(my_function_a(str))
+#
+#def my_function_e(x):
+#  strE = x.replace('e','3')
+#  return strE 
+#
+#strE = my_function_e(strA)
+#
+##print(my_function_e(strA))
+#  
+#def my_function_g(x):
+#  strG = x.replace('g','6')
+#  return strG 
+#
+#strG = my_function_g(strE)
+#
+##print(my_function_g(strE))
+#
+#def my_function_g(x):
+#  strI = x.replace('I','1')
+#  return strI 
+#
+#strI = my_function_g(strG)
+#
+##print(my_function_g(strG)
 
-def my_function_a(x):
-  strA = x.replace('a','4')
-  return strA
 
-strA = my_function_a(str)
+# S => 5
+# T => 7
 
-#print(my_function_a(str))
 
-def my_function_e(x):
-  strE = x.replace('e','3')
-  return strE 
+#
+#def my_function_g(x):
+#  strO = x.replace('o','0')
+#  return strO 
+#
+#strO = my_function_g(strI)
+#
+##print(my_function_g(strI))
+#
+#def my_function_g(x):
+#  strS = x.replace('s','5')
+#  return strS 
+#
+#strS = my_function_g(strO)
+#
+##print(my_function_g(strO))
+#
+#def my_function_g(x):
+#  strT = x.replace('t','7')
+#  return strT 
+#
+#strT = my_function_g(strS)
+#
+#print(my_function_g(strS))
 
-strE = my_function_e(strA)
-
-#print(my_function_e(strA))
-  
-def my_function_g(x):
-  strG = x.replace('g','6')
-  return strG 
-
-strG = my_function_g(strE)
-
-#print(my_function_g(strE))
-
-def my_function_g(x):
-  strI = x.replace('I','1')
-  return strI 
-
-strI = my_function_g(strG)
-
-#print(my_function_g(strG))
-
-def my_function_g(x):
-  strO = x.replace('o','0')
-  return strO 
-
-strO = my_function_g(strI)
-
-#print(my_function_g(strI))
-
-def my_function_g(x):
-  strS = x.replace('s','5')
-  return strS 
-
-strS = my_function_g(strO)
-
-#print(my_function_g(strO))
-
-def my_function_g(x):
-  strT = x.replace('t','7')
-  return strT 
-
-strT = my_function_g(strS)
-
-print(my_function_g(strS))
 
 # A => 4
 # E => 3
@@ -148,14 +155,49 @@ print(my_function_g(strS))
 # T => 7
 
 str = 'This is my absolute test string to see if my Leet cipher is working'
-original_character = ['A','a','E','e','G','g','I','i','O','o','S','s','T','t']
-new_character = ['4','4','3','3','6','6','1','1','0','0','5','5','7','7']
+#original_character = ['A','a','E','e','G','g','I','i','O','o','S','s','T','t']
+#new_character = ['4','4','3','3','6','6','1','1','0','0','5','5','7','7']
+#
+#index = 0
+#
+#while str[index] < len(str):
+#    while str[index] == str[original_character]
+#    return index
+#    print(str[index])
+#    index = index + 1
+
+str = 'This is my absolute test string to see if my Leet cipher is working'
+#original_character = ['A','a','E','e','G','g','I','i','O','o','S','s','T','t']
+#new_character = ['4','4','3','3','6','6','1','1','0','0','5','5','7','7']
+#
+#index = 0
+#
+#while str[index] < len(str):
+#    while str[index] == str[original_character]
+#    return index
+#    print(str[index])
+#    index = index + 1
+
+
+
+# def my_function_a(x):
+#  str1 = x.replace('a','4')
+#  str1 = str1.replace('e','3')
+#  str1 = str1.replace('g','6')
+#  str1 = str1.replace('I','1')
+#  str1 = str1.replace('o','0')
+#  str1 = str1.replace('s','5')
+#  str1 = str1.replace('t','7')
+#  print(str1)
+# my_function_a(str)
+
+## List Exercises
+
+list_to_sum = ['4','4','3','3','6','6','1','1','0','0','5','5','7','7']
 
 index = 0
+sum = 0
 
-while str[index] < len(str):
-    while str[index] == str[original_character]
-    return index
-    print(str[index])
-    index = index + 1
- 
+while index < len(list_to_sum):
+    sum =+ list_to_sum([index])
+    
